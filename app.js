c1449f257fb7fd6d6ec645d4583255abaa40ea54
MWei2 <MWei2@slb.com>
2016-12-25 18:10:47 +0800

    command for mock client

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
 const convert = require('koa-convert');
-const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 app.use(convert(bodyParser()));
