15da2c67eebaf5cdc60cf7ff106a2b796e2c4661
yinggeABC <906772533@qq.com>
2016-12-26 21:01:50 +0800

    去掉多余的文件引用

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@
  * Created by weimin on 16-12-25.
  */
 var router = require("koa-router")();
-var model = require('../model');
-var rp = require('request-promise');
-var fs = require('fs');
-var util = require('util');
-var User = require('../model').User;
 
 router.get("/test",async (ctx,next)=>{
     await ctx.render("index",{
