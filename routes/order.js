5bfcb2303af0056a5bc917cea079700453507cf3
weimeng <weimeng@ucfgroup.com>
2017-01-15 21:52:18 +0800

    支付接口参数顺序调整

--- routes/order.js
+++ routes/order.js
@@ -39,8 +39,9 @@ router.get('/order/sign/alipay', checkLogin,async(ctx, next) => {
     }
     //流水号暂时由时间戳与四位随机码生成
     alipayConfig.out_trade_no = Date.now().toString() + code;
-    var orderSpec = getParams(alipayConfig);
-    var sign = getSign(alipayConfig)
+
+    var orderSpec = getPayParams(alipayConfig);
+    var sign = getSign(orderSpec)
     ctx.body = {
         code:0,
         data:{
@@ -49,6 +50,53 @@ router.get('/order/sign/alipay', checkLogin,async(ctx, next) => {
         }
     };
 });
+
+function getPayParams(alipayConfig){
+    // 签约合作者身份ID
+    var orderInfo = "partner=" + "\"" + alipayConfig.partner + "\"";
+
+    // 签约卖家支付宝账号
+    orderInfo += "&seller_id=" + "\"" + alipayConfig.seller_id + "\"";
+
+    // 商户网站唯一订单号
+    orderInfo += "&out_trade_no=" + "\"" + alipayConfig.out_trade_no + "\"";
+
+    // 商品名称
+    orderInfo += "&subject=" + "\"" + alipayConfig.subject + "\"";
+
+    // 商品详情
+    orderInfo += "&body=" + "\"" + alipayConfig.body + "\"";
+
+    // 商品金额
+    orderInfo += "&total_fee=" + "\"" + alipayConfig.total_fee + "\"";
+
+    // 服务器异步通知页面路径
+    orderInfo += "&notify_url=" + "\"" + alipayConfig.notify_url + "\"";
+
+    // 服务接口名称， 固定值
+    orderInfo += "&service=\"mobile.securitypay.pay\"";
+
+    // 支付类型， 固定值
+    orderInfo += "&payment_type=\"1\"";
+
+    // 参数编码， 固定值
+    orderInfo += "&_input_charset=\"utf-8\"";
+
+    // 设置未付款交易的超时时间
+    // 默认30分钟，一旦超时，该笔交易就会自动被关闭。
+    // 取值范围：1m～15d。
+    // m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
+    // 该参数数值不接受小数点，如1.5h，可转换为90m。
+    orderInfo += "&it_b_pay=\"30m\"";
+
+    // extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
+    // orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
+
+    // 支付宝处理完请求后，当前页面跳转到商户指定页面的路径，可空
+    orderInfo += "&return_url=\"m.alipay.com\"";
+
+    return orderInfo;
+}
 
 function getParams(params){
     var sPara = [];
@@ -70,6 +118,22 @@ function getParams(params){
         }
     }
     return prestr;
+}
+
+function getPaySign(orderInfo) {
+    try {
+        var privatePem = fs.readFileSync('./pem/app_private.pem');
+        var key = privatePem.toString();
+        console.log(key);
+        var prestr = orderInfo; 
+        var crypto = require('crypto');
+        var sign = crypto.createSign('RSA-SHA1');
+        sign.update(prestr);
+        sign = sign.sign(key,'base64');
+        return encodeURIComponent(sign);
+    } catch(err) {
+        console.log('veriSign err', err)
+    } 
 }
 
 
